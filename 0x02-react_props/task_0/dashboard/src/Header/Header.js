a6d9729b685b3be7a2f78d1d11531eaecb196adb
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2024-11-05 08:10:33 +0000

    add

---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+  )
+}
+
+export default Header;
