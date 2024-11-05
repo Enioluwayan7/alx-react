7057982b67b48a911060d82e7aef3bc2209d2b04
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2024-11-05 10:27:43 +0000

    add

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Login from './Login/Login';
+import { Notifications } from './Notifications';
+import holbertonLogo from './holberton-logo.png';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        
+        {/* Email input */}
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+
+        {/* Password input */}
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+
+        {/* OK button */}
+        <button type="button">OK</button>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(false)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ 0x02-react_props/task_0/dashboard/src/App/Header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+  )
+}
+
+export default Header;
