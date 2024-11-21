1cfba45b42eaa62430212802cde3bb9ecc35e80c
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2024-11-21 19:08:50 +0000

    0-fromjs.js

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,25 +1,5 @@
-// Import Immutable.js
-const { fromJS } = require('immutable');
+import { fromJS } from 'immutable';
 
-/**
- * Converts a JavaScript object into an immutable Map.
- * @param {Object} object - The JavaScript object to convert.
- * @returns {Map} An immutable Map representation of the object.
- */
-
-function getImmutableObject(object) {
-    return fromJS(object);
+export default function getImmutableObject(object) {
+	  return fromJS(object);
 }
-
-// Example usage:
-const exampleObject = {
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-};
-
-const immutableMap = getImmutableObject(exampleObject);
-console.log(immutableMap);
-
-module.exports = getImmutableObject;
